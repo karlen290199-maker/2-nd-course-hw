8fe3fb76cce704aed3c367dee30e22f8dbc12350
karlen290199-maker <karlen290199@gmail.com>
2026-06-01 15:04:08 +0300

    Changed the file index.html.

--- script.js
+++ script.js
@@ -67,3 +67,12 @@ if (c > 0 && c < 10) {
 } else {
     console.log('Неверно');
 }
+
+let d = 150;
+let e = 80;
+
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
