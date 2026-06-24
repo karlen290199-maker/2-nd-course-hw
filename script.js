9f7a447624b60eec6da2543f1f2d9e7760c01adb
karlen290199-maker <karlen290199@gmail.com>
2026-06-24 11:23:03 +0300

    Changed the file index.html.

--- script.js
+++ script.js
@@ -328,3 +328,12 @@ function startQuiz() {
     }
     alert(resultMessage);
 }
+
+const arr = [1, 5, 4, 10, 0, 3];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    if (arr[i] === 10) {
+        break;
+    }
+}
