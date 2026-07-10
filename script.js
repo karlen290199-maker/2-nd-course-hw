735360b18f2aaa8a31a79b8d53e7bb594f3cfad6
karlen290199-maker <karlen290199@gmail.com>
2026-07-10 20:24:02 +0300

    Changed the file index.html.

--- script.js
+++ script.js
@@ -784,3 +784,16 @@ function delayForSecond(callback) {
 delayForSecond(function () {
    console.log('Привет, Глеб!');
 });
+
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
+}
+
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
+
+delayForSecond(() => sayHi('Глеб'));
