58934a825befc25eea5673f9905cf71d3f0cfcfa
karlen290199-maker <karlen290199@gmail.com>
2026-07-10 20:17:17 +0300

    Changed the file index.html.

--- script.js
+++ script.js
@@ -725,12 +725,40 @@ function playGame() {
 
 playGame();
 
-const people = [
+const peopleForSort = [
    { name: 'Глеб', age: 29 },
    { name: 'Анна', age: 17 },
    { name: 'Олег', age: 7 },
    { name: 'Оксана', age: 47 }
 ];
 
-console.log(people.sort((a, b) => a.age - b.age));
+console.log(peopleForSort.sort((a, b) => a.age - b.age));
 
+function isPositive(num) {
+    return num > 0;
+}
+
+function isMale(person) {
+    return person.gender === 'male';
+}
+
+function filter(array, ruleFunction) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (ruleFunction(array[i], i, array)) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const peopleForFilter = [
+   {name: 'Глеб', gender: 'male'},
+   {name: 'Анна', gender: 'female'},
+   {name: 'Олег', gender: 'male'},
+   {name: 'Оксана', gender: 'female'}
+];
+
+console.log(filter(peopleForFilter, isMale));
