6bd1eb04dd4bd803d142a42b28e1a6e46c94a4cf
karlen290199-maker <karlen290199@gmail.com>
2026-07-18 19:07:34 +0300

    Changed the file index.html.

--- script.js.hw9.js
+++ script.js.hw9.js
@@ -1,14 +1,24 @@
 const header = document.querySelector('.header');
-        const button = document.querySelector('.button');
+const button = document.querySelector('.button');
+const paragraph = document.querySelector("#colorParagraph");
+const colorBtn = document.querySelector("#changeColorBtn");
 
-        button.addEventListener('click', function() {
-            header.classList.toggle('header--hidden');
+
+button.addEventListener('click', function() {
+    header.classList.toggle('header--hidden');
             
-            if (header.classList.contains('header--hidden')) {
-                button.textContent = 'Показать';
-                button.classList.add('button--active');
-            } else {
-                button.textContent = 'Скрыть';
-                button.classList.remove('button--active');
-            }
-        });
+    if (header.classList.contains('header--hidden')) {
+        button.textContent = 'Показать';
+        button.classList.add('button--active');
+    } else {
+        button.textContent = 'Скрыть';
+        button.classList.remove('button--active');
+    }
+});
+
+let isBlue = false;
+    colorBtn.addEventListener("click", function () {
+    isBlue = !isBlue;
+    paragraph.style.color = isBlue ? "#1e3a8a" : "#334155"; 
+    colorBtn.textContent = isBlue ? "Вернуть цвет" : "Изменить цвет";
+});
